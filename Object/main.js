ca685dd45b77b35d651c25c780fd3ac9299a794e
Linh Nguyen <nguyenthuylinh040205@gmail.com>
2023-07-29 15:32:20 +0700

    new changes

---
+++ Object/main.js
@@ -0,0 +1,46 @@
+/* /*OBJECT*/
+/* var schoolKey = 'school'
+var myInfo ={
+    name: 'Linh',
+    age:18,
+    address:'Ha Noi',
+    [schoolKey] : 'HUST',
+    getAge: function(){
+        return this.age
+    }
+}
+console.log(myInfo); */
+/*Thêm key và value*/
+/* myInfo.email = 'Linhnguyen@.email'
+console.log(myInfo);
+var myKey = 'name';
+console.log(myInfo[myKey]) */
+
+/*Thêm key và value cách 2*/
+/* var schoolKey = 'school' */ /*phải khai báo trc myInfo*/
+
+/* console.log(myInfo) */
+/*Cách xóa*/
+/* delete myInfo.name
+console.log(myInfo)
+getName = function(){
+    return myInfo.age
+} */
+/* console.log(myInfo.getAge()) */
+//Function --> Phương thức method
+// Other ---> Thuộc tính property */
+
+
+
+
+//Object constructor
+
+function User(firstName, lastName, avatar){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.avatar = avatar;
+}
+var author = new User('Linh','Nguyễn','img');
+var user = new User('Linh','Nguyễn','Thuy');
+console.log(author);
+console.log(user);
---
+++ lesson_1/main.js
@@ -0,0 +1,19 @@
+/**hàm built-in
+alert 
+console 
+confirm
+prompt
+set setTimeout
+set setInterval*/
+
+var fullName = "Nguyen Thuy Linh";
+var age = 26;
+
+console.log(fullName)
+if(age>16){
+    console.warn(fullName)
+}
+var a=3
+var b=2
+var c=a/b
+console.log(c)
