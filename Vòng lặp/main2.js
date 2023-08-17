2d717b25c9465ceb5a54439a0125190c91da1ee2
Linh Nguyen <nguyenthuylinh040205@gmail.com>
2023-08-17 23:34:11 +0700

    push reduce excersise

--- Vòng lặp/main2.js
+++ Vòng lặp/main2.js
@@ -1,22 +1,163 @@
-var sportsGold = [
+//1) Turn an array of numbers into a total of all the numbers
+function total(arr) {
+   var myTotal = arr.reduce(function(a,b){
+    return a+b
+   },0)
+   return myTotal
+}
+
+console.log(total([1,2,3])); // 6
+ 
+//2) Turn an array of numbers into a long string of all those numbers.
+function stringConcat(arr) {
+   // your code here 
+   return arr.reduce(function(a,b){
+    return `${a}${b}`
+   },'')
+   
+}
+
+console.log(stringConcat([1,2,3])); // "123"
+
+//3) Turn an array of voter objects into a count of how many people voted
+function totalVotes(arr) {
+   // your code here    
+}
+
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+function totalVotes(voters){
+    return voters.reduce(function(total, number){
+        if(number.voted == 'true'){
+            return total + 1
+    
+        }
+    },0)
+} 
+console.log(totalVotes(voters)); // 7
+
+
+// Bài reduce khó
+/* Cho trước danh sách một số bộ phim, hãy viết hàm calculateRating để tính điểm trung bình IMDB của những bộ phim mà Christopher Nolan làm đạo diễn.
+
+Gợi ý
+Dùng phương thức filter để lấy ra những bộ phim do Christopher Nolan làm đạo diễn
+Dùng phương thức reduce để tính tổng điểm IMDB
+Tính điểm IMDB trung bình */
+var watchList = [
     {
-        name: 'Bơi lội',
-        gold: 11
+      "Title": "Inception",
+      "Year": "2010",
+      "Rated": "PG-13",
+      "Released": "16 Jul 2010",
+      "Runtime": "148 min",
+      "Genre": "Action, Adventure, Crime",
+      "Director": "Christopher Nolan",
+      "Writer": "Christopher Nolan",
+      "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+      "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+      "Language": "English, Japanese, French",
+      "Country": "USA, UK",
+      "imdbRating": "8.8",
+      "imdbVotes": "1,446,708",
+      "imdbID": "tt1375666",
+      "Type": "movie",
     },
     {
-        name: 'Boxing',
-        gold: 3
+      "Title": "Interstellar",
+      "Year": "2014",
+      "Rated": "PG-13",
+      "Released": "07 Nov 2014",
+      "Runtime": "169 min",
+      "Genre": "Adventure, Drama, Sci-Fi",
+      "Director": "Christopher Nolan",
+      "Writer": "Jonathan Nolan, Christopher Nolan",
+      "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+      "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      "Language": "English",
+      "Country": "USA, UK",
+      "imdbRating": "8.6",
+      "imdbVotes": "910,366",
+      "imdbID": "tt0816692",
+      "Type": "movie",
     },
     {
-        name: 'Đạp xe',
-        gold: 4
+      "Title": "The Dark Knight",
+      "Year": "2008",
+      "Rated": "PG-13",
+      "Released": "18 Jul 2008",
+      "Runtime": "152 min",
+      "Genre": "Action, Adventure, Crime",
+      "Director": "Christopher Nolan",
+      "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+      "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+      "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+      "Language": "English, Mandarin",
+      "Country": "USA, UK",
+      "imdbRating": "9.0",
+      "imdbVotes": "1,652,832",
+      "imdbID": "tt0468569",
+      "Type": "movie",
     },
     {
-        name: 'Đấu kiếm',
-        gold: 5
+      "Title": "Batman Begins",
+      "Year": "2005",
+      "Rated": "PG-13",
+      "Released": "15 Jun 2005",
+      "Runtime": "140 min",
+      "Genre": "Action, Adventure",
+      "Director": "Christopher Nolan",
+      "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+      "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+      "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+      "Language": "English, Urdu, Mandarin",
+      "Country": "USA, UK",
+      "imdbRating": "8.3",
+      "imdbVotes": "972,584",
+      "imdbID": "tt0372784",
+      "Type": "movie",
     },
-]
-var myTotalGold = sportsGold.reduce(function(accumulator,currentValue){
-    return accumulator +currentValue
-},0);
-console.log('this is' + myTotalGold)
+    {
+      "Title": "Avatar",
+      "Year": "2009",
+      "Rated": "PG-13",
+      "Released": "18 Dec 2009",
+      "Runtime": "162 min",
+      "Genre": "Action, Adventure, Fantasy",
+      "Director": "James Cameron",
+      "Writer": "James Cameron",
+      "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+      "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+      "Language": "English, Spanish",
+      "Country": "USA, UK",
+      "imdbRating": "7.9",
+      "imdbVotes": "876,575",
+      "imdbID": "tt0499549",
+      "Type": "movie",
+    }
+  ];
+  
+  function calculateRating(arr){
+    var chrisFilm =  arr.filter(function(film){
+      return film.Director==='Christopher Nolan'
+    });
+    var totalRate = chrisFilm.reduce(function(total,film){
+      return total + parseFloat(film.imdbRating) //  parses a value as a string and returns the first number.
+    },0)
+    var imdbAverage = totalRate / chrisFilm.length;
+    return imdbAverage}
+    
+  // Expected results
+  console.log(calculateRating(watchList)); // Output: 8.675
